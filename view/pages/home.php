97886a380f5b39e7598e97856f790e3ef8b0e42f
KingW3n <wendeljr01@gmail.com>
2021-03-09 01:58:48 -0300

    home

--- view/pages/home.php
+++ view/pages/home.php
@@ -4,19 +4,58 @@
 	<title>Home - Meu Chá</title>
 	<link rel="stylesheet" type="text/css" href="../../assets/estilo.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" ></script>
+   	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../../assets/script.js"></script>
 
 </head>
 <body onload="">
 	<!-- Sidebar -->
 		<div class="sidebar">
-		
+			<div id="imageLogo">
+				<img src="../../img/LogoMeucha.png" width="100%">
+			</div>
+			<ul class="menuVertical">
+				<div class="sidebar-heading">
+                Eventos
+            </div>
+            <li>
+             <a href="#" data-toggle="collapse" data-target="#submenu-1">
+				<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard-plus" viewBox="0 0 16 16">
+  							<path fill-rule="evenodd" d="M8 7a.5.5 0 0 1 .5.5V9H10a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V10H6a.5.5 0 0 1 0-1h1.5V7.5A.5.5 0 0 1 8 7z"/>
+  							<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+  							<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/><span> Novo evento </span>
+						</svg>
+                	</a>
+            	</li>
+            	<li>
+                	<a href="#" data-toggle="collapse" data-target="#submenu-1">
+                		<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-clipboard" viewBox="0 0 16 16">
+  							<path d="M4 1.5H3a2 2 0 0 0-2 2V14a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V3.5a2 2 0 0 0-2-2h-1v1h1a1 1 0 0 1 1 1V14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V3.5a1 1 0 0 1 1-1h1v-1z"/>
+  							<path d="M9.5 1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-3a.5.5 0 0 1-.5-.5v-1a.5.5 0 0 1 .5-.5h3zm-3-1A1.5 1.5 0 0 0 5 1.5v1A1.5 1.5 0 0 0 6.5 4h3A1.5 1.5 0 0 0 11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3z"/>
+  							<span> Meus eventos </span>
+						</svg>
+                	</a>
+            	</li>
+            <li>
+                <a href="#" data-toggle="modal" data-target="#deslogar">
+                    <i class="fa fa-sign-out"></i><span>Sair</span>
+                </a>
+            </li>
+        </ul>
 		</div>
 		<div class="topSidebar"></div>
 		<div class="conteudo"></div>
-		<div class="footer"></div>
+		<div class="footer">
+			 <div class="copyright text-center my-auto">
+            <span>Copyright © Your Website 2020</span>
+        </div>
+		</div>
 	
 </body>
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+       
+    </div>
+</footer>
 </html>
