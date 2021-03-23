59aee726044b8d380b2b3bdac0126ad8320e51b7
KingW3n <wendeljr01@gmail.com>
2021-03-23 11:24:31 -0300

    atualização do dia 22

--- assets/script.js
+++ assets/script.js
@@ -64,27 +64,35 @@ function funcNovoEvento(){
             $(".divNomeBebe").hide();
         }
     })
-   
-
-   
 }
 
 
 function ConfigTenaNovoEventoChaRevelacao(){
-    $("#inputNomeDele,#inputNomedoBebe,#inputNomeDele,#inputNomeDela").keydown(function(){
-        $("#conteudoEventoConvite").html(
-            '<center>'+
-                '<h1 class="fontPadraoSite">CHÁ REVELAÇÃO</h1>'+
-                '<p class="width90">Estamos ansiosos para saber o sexo do nosso bebê, convidamos você para esse momento tao especial onde saberemos se é um menino ou menina!</p>'+
-                '<p>os nomes ja sabemos será:'+$("#inputNomedoBebe").val()+'</p>'+
-                '<h2 class="fontPadraoSite">NO DIA</h2>'+
-                '<h1 class="fontPadraoSite"></h1>'+
-                '<p>Os papais: '+$("#inputNomeDela").val()+' && '+$("#inputNomeDele").val()+' </p>'+
-                '<p>contam com a sua presença</p>'+
-            '</center>'
-        );
-       
-    })
+    let dia="";
+    let mes ="";
+    let hora ="";
+    let mesExtenso ="";
+    let mesExtensoArray = new Array ("janeiro", "fevereiro", "março", "abril", "maio", "junho", "agosto", "outubro", "novembro", "dezembro");;
+    $("#inputNomeDele,#inputNomedoBebe,#inputNomeDele,#inputNomeDela,#inputDataEvento,#inputHora").keydown(function(){preencherConvite(dia,mesExtenso,hora)});
+    $("#inputDataEvento, #inputHora").on("change", function(){
+        let data = new Date($("#inputDataEvento").val().replace(/-/g,'/')), dia = data.getDate(), mes  = data.getMonth(), ano  = data.getFullYear();
+        if (dia <=9){ dia="0"+dia}
+        if(mes!=""){mesExtenso = mesExtensoArray[mes]}else{mesExtenso = "";}
+        preencherConvite(dia,mesExtenso,hora);
+    });  
+}
+function preencherConvite(dia,mesExtenso,hora){
+    $("#conteudoEventoConvite").html(
+        '<center>'+
+        '<h1 class="fontPadraoSite">CHÁ REVELAÇÃO</h1>'+
+        '<p class="width90">Estamos ansiosos para saber o sexo do nosso bebê, convidamos você para esse momento tao especial onde saberemos se é um menino ou menina!</p>'+
+        '<p>os nomes ja sabemos será:'+$("#inputNomedoBebe").val()+'</p>'+
+        '<label><h5 class="fontPadraoSite">NO DIA   </h5> </label> <label><h1 class="fontPadraoSite">'+dia+'</h1></label><label><h6 class="fontPadraoSite"> de '+mesExtenso+'</h6> </label>'+
+        '<h3 class="fontPadraoSite"> as '+hora+' Hrs</h3>'+
+        '<p>Os papais: '+$("#inputNomeDela").val()+' && '+$("#inputNomeDele").val()+' </p>'+
+        '<p>contam com a sua presença</p>'+
+        '</center>'
+    );
 }
 
 function VerificarCampCadastro(){
